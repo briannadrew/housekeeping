dbd2a7694e8556036b93a2f31c888976563ef625
Brianna Drew <44736324+briannadrew@users.noreply.github.com>
2023-11-03 06:32:54 -0700

    Update wallpaper.py
    Added functionality to handle saving of invalid image files

--- wallpaper.py
+++ wallpaper.py
@@ -3,9 +3,10 @@ import os.path
 import requests
 import json
 import ctypes
+import filetype
 
 SPI_SETDESKWALLPAPER = 20
-FOLDER = "D:/wallpapers/" # folder path
+FOLDER = "D:/wallpapers/img" # folder path
 
 # function to check if the image has previously been used
 def check_unique(fileSearch):
@@ -14,6 +15,14 @@ def check_unique(fileSearch):
         if(fileSearch in files):
             unique = False # if the file was found in the wallpaper folder already, it is not unique
     return unique
+
+# function to delete all non-image files that were downloaded in the process
+def del_files():
+    for root, dir, files in os.walk(FOLDER): # search through existing files in the wallpaper folder
+        for filename in files:
+            curr_path = os.path.join(root, filename) # get full path of current file
+            if not filetype.is_image(curr_path):
+                os.remove(curr_path) # if the current file is not an image, remove it
 
 # supply user agent in the header so it will be recognized by the server
 myHeaders={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"}
@@ -36,4 +45,8 @@ if(unique):
     completeName = os.path.join(FOLDER, imgname) # path to where the image will be saved
     with open(completeName, "wb") as imageFile:
         imageFile.write(imageContents) # save the image to wallpapers folder
-    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, completeName, 3) # set the image as the desktop background
+    if filetype.is_image(completeName): # if the file downloaded is a valid image file
+        ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, completeName, 3) # set the image as the desktop background
+        del_files() # call function to delete non-image files in the directory
+    else:
+        os.system('python wallpaper.py') # restart the script
